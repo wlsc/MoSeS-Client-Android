423c6a20121979e490f29069dee17163515ec1ed
Simon_Leischnig <simonjena@gmail.com>
2012-01-25 20:50:49 +0100

    missing file

---
+++ moses/src/moses/client/userstudy/UserstudyNotificationManager.java
@@ -0,0 +1,115 @@
+package moses.client.userstudy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import moses.client.util.FileLocationUtil;
+import android.content.Context;
+
+public class UserstudyNotificationManager {
+	private static UserstudyNotificationManager instance;
+	private List<UserStudyNotification> notifications;
+	
+	/**
+	 * initializes the manager (if there is a file that contains an old manager,
+	 * this will be loaded; otherwise, an empty manager will be created.
+	 * 
+	 * @param appContext
+	 *            the application context
+	 * @throws IOException
+	 *             if something goes wrong with reading/parsing the manager
+	 *             file.
+	 */
+	public static void init(Context appContext) throws IOException {
+		instance = loadInstance(appContext);
+	}
+	
+	public static UserstudyNotificationManager getInstance() {
+			return instance;
+	}
+	
+	public UserstudyNotificationManager() {
+		this.notifications = new LinkedList<UserStudyNotification>();
+	}
+
+	/**
+	 * Saves all managed references to external apps to the default file (see
+	 * Util.getAppdatabaseFile)
+	 * 
+	 * @param appContext
+	 * @throws IOException
+	 *             if there went something wrong at opening or writing the file.
+	 */
+	public void saveToDisk(Context appContext) throws IOException {
+		FileWriter writer;
+		BufferedWriter bufWriter = null;
+		try {
+			writer = new FileWriter(FileLocationUtil.getAppDatabaseFile(appContext));
+			bufWriter = new BufferedWriter(writer);
+			for (UserStudyNotification notification : notifications) {
+//				bufWriter.append(notification.asOnelineString() + "\n");TODO:
+			}
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			try {
+				if (bufWriter != null) bufWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * loads an InstalledExternalApplicationsManager from the standard file (see
+	 * Util.getAppdatabaseFile). If this file does not exist, this method
+	 * returns a manager with zero initial references.
+	 * 
+	 * @param context
+	 * @return the loaded manager
+	 * @throws IOException
+	 */
+	public static UserstudyNotificationManager loadInstance(Context context) throws IOException {
+		File settingsFile = FileLocationUtil.getNotificationDatabaseFile(context);
+		UserstudyNotificationManager manager = new UserstudyNotificationManager();
+		if (settingsFile.exists()) {
+			FileReader reader = null;
+			BufferedReader bufReader = null;
+			try {
+				reader = new FileReader(settingsFile);
+				bufReader = new BufferedReader(reader);
+				String line;
+				while ((line = bufReader.readLine()) != null) {
+					if (!line.trim().equals("")) {
+//						UserStudyNotification notification = UserStudyNotification.fromOnelineString(line);TODO:
+//						manager.addNotification(notification);//TODO:
+					}
+				}
+				return manager;
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return new UserstudyNotificationManager();
+			} catch (IOException e) {
+				throw e;
+			} finally {
+				if (reader != null) {
+					if (bufReader != null) bufReader.close();
+				}
+			}
+		} else {
+			return new UserstudyNotificationManager();
+		}
+	}
+
+	public void addNotification(UserStudyNotification notification) {
+		notifications.add(notification);
+		//TODO: notify view?
+	}
+}
