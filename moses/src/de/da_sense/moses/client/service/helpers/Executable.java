5615e107650a1acc25c1dad12bb80eb19068b271
Florian Schnell <fs.jura@gmail.com>
2012-07-28 15:10:14 +0200

    [Client] Executor => Executeable; Following JavaNamingConvention

---
+++ moses/src/de/da_sense/moses/client/service/helpers/Executable.java
@@ -0,0 +1,15 @@
+package de.da_sense.moses.client.service.helpers;
+
+// TODO: Auto-generated Javadoc
+/**
+ * Interface, consisting of a non parametric function execute
+ * 
+ * @author Jaco Hofmann
+ */
+public interface Executable {
+
+	/**
+	 * Execute.
+	 */
+	public void execute();
+}
---
+++ moses/src/de/da_sense/moses/client/service/helpers/ExecutableForObject.java
@@ -0,0 +1,14 @@
+package de.da_sense.moses.client.service.helpers;
+/**
+ * 
+ * Interface Executable with method execute(Object o)
+ * @author Jaco Hofmann
+ * 
+ */
+public interface ExecutableForObject {
+	/**
+	 * Method specified by all classes which implement this interface
+	 * @param o
+	 */
+	public void execute(Object o);
+}
