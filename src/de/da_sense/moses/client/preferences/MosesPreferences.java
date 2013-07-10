9dfd3a24ac84f63f212006685f4ac79eeab8f4e7
Zijad Maksuti <zijad.maksuti@gmail.com>
2013-07-10 16:08:00 +0200

    * [Clien] EditText does not disappear when screen orientation changes in preferences

--- src/de/da_sense/moses/client/preferences/MosesPreferences.java
+++ src/de/da_sense/moses/client/preferences/MosesPreferences.java
@@ -26,15 +26,8 @@ public class MosesPreferences extends PreferenceActivity {
 		ActionBar ab = getActionBar();
 		ab.setTitle("Settings");
 		ab.setDisplayShowTitleEnabled(true);
-		
 		getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
 	}
-	
-	
-	
-//	public void onBuildHeaders(List<Header> target){
-//		loadHeadersFromResource(R.xml.preference_headers, target);
-//	}
 
 	/**
 	 * @see android.app.Activity#onWindowFocusChanged(boolean)
