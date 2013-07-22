56808379c74a72895500a38cbf5968421397b8a9
Zijad Maksuti <zijad.maksuti@gmail.com>
2013-07-22 20:24:21 +0200

    +[Client] User is not able to enter a non valid deviceID in preference screen until he enters a valid one.

--- src/de/da_sense/moses/client/preferences/MosesPreferences.java
+++ src/de/da_sense/moses/client/preferences/MosesPreferences.java
@@ -1,6 +1,8 @@
 package de.da_sense.moses.client.preferences;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
@@ -15,6 +17,9 @@ import android.text.Spanned;
 import android.text.TextWatcher;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import de.da_sense.moses.client.R;
 import de.da_sense.moses.client.service.MosesService;
@@ -59,6 +64,9 @@ public class MosesPreferences extends PreferenceActivity {
 	public static class PrefsFragment extends PreferenceFragment {
 		
 		private static String LOG_TAG = PreferenceFragment.class.getSimpleName();
+		
+		private Button mPositiveButtonDeviceId;
+		
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
@@ -69,7 +77,7 @@ public class MosesPreferences extends PreferenceActivity {
 			getActivity();
 			
 			/*
-			 * Change the default behaviour of the EditTextPreference for changing the deviceId
+			 * Change the default behavior of the EditTextPreference for changing the deviceId
 			 */
 			final EditTextPreference prefDevId = (EditTextPreference) findPreference(getString(R.string.deviceIDPreferenceKey));
 			final EditText devIdEditText = prefDevId.getEditText();
@@ -80,9 +88,31 @@ public class MosesPreferences extends PreferenceActivity {
 					// disable suggestions and spell checking
 					devIdEditText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
 					devIdEditText.setSelection(devIdEditText.getText().length());
+					
+					/*
+					 * prevent the preference dialog from closing if the user clicks on ok
+					 * and entered data could not be validated
+					 */
+					final Dialog dialog = prefDevId.getDialog();
+					final AlertDialog alertDialog = (AlertDialog) dialog; // a trick for getting controls over the button
+					mPositiveButtonDeviceId = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+					
+					mPositiveButtonDeviceId.setOnClickListener(new OnClickListener() {
+						
+						public void onClick(View v) {
+							// trim the device name entered by he user, before persisting and sending it to the server
+							devIdEditText.setText(devIdEditText.getText().toString().trim());
+							prefDevId.onClick(dialog, AlertDialog.BUTTON_POSITIVE);
+//							prefDevId.onDismiss(dialog);
+							dialog.dismiss();
+						}
+					});
+					
 					return true;
 				}
+				
 			});
+			
 			
 			// give feedback to the user if he enters a non valid character
 			devIdEditText.addTextChangedListener(new TextWatcher() {
@@ -101,34 +131,50 @@ public class MosesPreferences extends PreferenceActivity {
 				@Override
 				public void afterTextChanged(Editable s) {
 					// show error message if user entered non valid character
-					// only alphanumeric characters and and underscores are allowed
+					// only alphanumeric characters and underscores are allowed
+					String errorMessage = null;
 					boolean isErrorFound = false;
+					String input ="";
 					for(int i=0; i<s.length(); i++){
 						char ch = s.charAt(i);
+						input+=ch;
 	                	// only alphanumeric characters and underscores allowed
-                        if (!Character.isLetterOrDigit(ch) && ch != '_' && ch != ' ') {
+                        if (!Character.isLetterOrDigit(ch) && ch != '_' && ch!=' ') {
                         	ForegroundColorSpan redSpan = new ForegroundColorSpan(Color.RED);
                         	s.setSpan(redSpan, i, i+1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                         	isErrorFound = true;
+                        	errorMessage = getString(R.string.dev_id_invalid_character_error);
                         	}
                         }
-					if(isErrorFound)
-						devIdEditText.setError(getString(R.string.dev_id_invalid_character_error));
-					else
-						devIdEditText.setError(null);
+					if(input.trim().length() == 0){
+						isErrorFound = true;
+						errorMessage = getString(R.string.dev_id_empty_error);
+					}
+					devIdEditText.setError(errorMessage);
+					
+					/*
+					 *  There is no guarantee that mPositiveButtonDeviceId is initialized on the first run
+					 *  thus this check is made. 
+					 */
+					if(mPositiveButtonDeviceId != null)
+						mPositiveButtonDeviceId.setEnabled(!isErrorFound);
 				}
 			});
-						
-			prefDevId.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-				
-				@Override
-				public boolean onPreferenceChange(Preference preference, Object newValue) {
-					Log.d(LOG_TAG, preference.toString());
-					Log.d(LOG_TAG, newValue.toString());
-					return false;
-				}
-			});
+			
+//			prefDevId.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+//
+//				@Override
+//				public boolean onPreferenceChange(Preference preference, Object newValue) {
+//					String newValueTrimmed = ((String)newValue).trim();
+//					prefDevId.setText(newValueTrimmed);
+//					Log.d(LOG_TAG, preference.toString());
+//					Log.d(LOG_TAG, newValueTrimmed.toString());
+//					return true;
+//				}
+//			});
+			
 		}
+		
 		
 	}
 	
