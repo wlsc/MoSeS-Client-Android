e02f5809e22383fa766dcbc4f484184960d2f73b
Zijad Maksuti <zijad.maksuti@gmail.com>
2013-07-23 18:17:40 +0200

    [Client]* Error hint on preferences disappears as soon as the user starts typing *transfered deviceID is trimmed

--- src/de/da_sense/moses/client/preferences/MosesPreferences.java
+++ src/de/da_sense/moses/client/preferences/MosesPreferences.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
@@ -15,7 +16,7 @@ import android.text.InputType;
 import android.text.Spanned;
 import android.text.TextWatcher;
 import android.text.style.ForegroundColorSpan;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -39,11 +40,9 @@ public class MosesPreferences extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Actionbar
-		ActionBar actionBar = getActionBar();
-		actionBar.setTitle("Settings");
-		actionBar.setDisplayShowTitleEnabled(true);
-		actionBar.setDisplayHomeAsUpEnabled(true);
-		
+		ActionBar ab = getActionBar();
+		ab.setTitle("Settings");
+		ab.setDisplayShowTitleEnabled(true);
 		getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
 	}
 
@@ -58,15 +57,6 @@ public class MosesPreferences extends PreferenceActivity {
 		}
 	}
 	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == android.R.id.home) {
-	        onBackPressed();
-	        return true;
-	    }
-	    return super.onOptionsItemSelected(item);
-	}
-	
 	
 	/*
 	 * Fragment
@@ -76,6 +66,7 @@ public class MosesPreferences extends PreferenceActivity {
 		private static String LOG_TAG = PreferenceFragment.class.getSimpleName();
 		
 		private Button mPositiveButtonDeviceId;
+		private String mErrorDeviceIdErrorMessage;
 		
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
@@ -98,23 +89,27 @@ public class MosesPreferences extends PreferenceActivity {
 					// disable suggestions and spell checking
 					devIdEditText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
 					devIdEditText.setSelection(devIdEditText.getText().length());
-					
 					/*
 					 * prevent the preference dialog from closing if the user clicks on ok
 					 * and entered data could not be validated
 					 */
 					final Dialog dialog = prefDevId.getDialog();
+					
 					final AlertDialog alertDialog = (AlertDialog) dialog; // a trick for getting controls over the button
 					mPositiveButtonDeviceId = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
 					
 					mPositiveButtonDeviceId.setOnClickListener(new OnClickListener() {
 						
 						public void onClick(View v) {
-							// trim the device name entered by he user, before persisting and sending it to the server
-							devIdEditText.setText(devIdEditText.getText().toString().trim());
-							prefDevId.onClick(dialog, AlertDialog.BUTTON_POSITIVE);
-//							prefDevId.onDismiss(dialog);
-							dialog.dismiss();
+							if(mErrorDeviceIdErrorMessage != null){
+								devIdEditText.setError(mErrorDeviceIdErrorMessage);
+							}
+							else{
+								// trim the device name entered by he user, before persisting and sending it to the server
+								devIdEditText.setText(devIdEditText.getText().toString().trim());
+								prefDevId.onClick(dialog, AlertDialog.BUTTON_POSITIVE);
+								dialog.dismiss();
+							}
 						}
 					});
 					
@@ -122,6 +117,7 @@ public class MosesPreferences extends PreferenceActivity {
 				}
 				
 			});
+			
 			
 			
 			// give feedback to the user if he enters a non valid character
@@ -140,10 +136,10 @@ public class MosesPreferences extends PreferenceActivity {
 				
 				@Override
 				public void afterTextChanged(Editable s) {
-					// show error message if user entered non valid character
+					// mark invalid characters with red
 					// only alphanumeric characters and underscores are allowed
-					String errorMessage = null;
-					boolean isErrorFound = false;
+					mErrorDeviceIdErrorMessage = null;
+					devIdEditText.setError(null); // remove previous error as soon as the user starts typing
 					String input ="";
 					for(int i=0; i<s.length(); i++){
 						char ch = s.charAt(i);
@@ -152,22 +148,12 @@ public class MosesPreferences extends PreferenceActivity {
                         if (!Character.isLetterOrDigit(ch) && ch != '_' && ch!=' ') {
                         	ForegroundColorSpan redSpan = new ForegroundColorSpan(Color.RED);
                         	s.setSpan(redSpan, i, i+1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        	isErrorFound = true;
-                        	errorMessage = getString(R.string.dev_id_invalid_character_error);
+                        	mErrorDeviceIdErrorMessage = getString(R.string.dev_id_invalid_character_error);
                         	}
                         }
 					if(input.trim().length() == 0){
-						isErrorFound = true;
-						errorMessage = getString(R.string.dev_id_empty_error);
+						mErrorDeviceIdErrorMessage = getString(R.string.dev_id_empty_error);
 					}
-					devIdEditText.setError(errorMessage);
-					
-					/*
-					 *  There is no guarantee that mPositiveButtonDeviceId is initialized on the first run
-					 *  thus this check is made. 
-					 */
-					if(mPositiveButtonDeviceId != null)
-						mPositiveButtonDeviceId.setEnabled(!isErrorFound);
 				}
 			});
 			
